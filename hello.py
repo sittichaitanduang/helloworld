98e35cf6bf3bb70846d03cf798c64fa14f394b49
Sittichai Tanduang <sittichai@mfu.ac.th>
2026-05-12 21:07:10 +0700

    update sailboat to new one

--- hello.py
+++ hello.py
@@ -1,13 +1,59 @@
-print('Hello World')
+from time import sleep
+import os
 
-text='''
-                ^
-               / \
-              /___\
-             |=   =|
-             |     |
-         ___/|_____|\___
-        /::::::::::::::::\
-~~~~~~~::::::::::::::::::::~~~~~~~
-'''
-print(text)
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+rocket_frames = [
+r"""
+               /\
+              /  \
+             /____\
+             |    |
+             |NASA|
+             |    |
+            /| || |\
+           /_|_||_|_\
+              /||\
+             /_||_\
+            /_/  \_\
+""",
+r"""
+               /\
+              /  \
+             /____\
+             |    |
+             |NASA|
+             |    |
+            /| || |\
+           /_|_||_|_\
+              /||\
+             /_||_\
+            /_/  \_\
+             **  **
+""",
+r"""
+               /\
+              /  \
+             /____\
+             |    |
+             |NASA|
+             |    |
+            /| || |\
+           /_|_||_|_\
+              /||\
+             /_||_\
+            /_/  \_\
+            ****** 
+""",
+]
+
+print("\n🚀 Launching Rocket...\n")
+
+for i in range(6):
+    clear()
+    print("\n" * (5 - i))  # ทำให้จรวดลอยขึ้น
+    print(rocket_frames[i % len(rocket_frames)])
+    sleep(0.3)
+
+print("\n✨ Mission Complete! ✨")
